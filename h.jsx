3064379cc08598fbaf8a0804996a981aaa08e40a
ARUNKUMARO15 <113664088+ARUNKUMARO15@users.noreply.github.com>
2023-09-20 22:37:50 +0530

    Add files via upload

---
+++ h.jsx
@@ -0,0 +1,150 @@
+import './h.css';
+
+
+function Arun()
+{
+    return (
+        <div className="blocks">
+            <div className="b1">
+                <div className='b1-container'>
+
+                <div id="b1c">
+              <div className="b1text">
+                <h4 style={{color:'red',textAlign:'left',fontFamily:'sans-serif'}}>CertiChain</h4>
+                <h1 style={{color:'white',textAlign:'left',fontFamily:'revert-layer'}}>Be Safe</h1>
+                <h1 style={{color:'white',textAlign:'left',fontFamily:'revert-layer'}}>Be Smart</h1>
+                <h5 style={{textAlign:'left',fontFamily:'sans-serif'}}>Save your Evidences Now !!</h5>
+                <button id="qbtn" >View Quizzes</button>
+              </div>
+              <div id="b1pic">
+                <img src="https://mecaluxcom.cdnwm.com/blog/img/blockchain-exchange-industrial-information.1.2.jpg" height="400px" width="600px"></img>
+              </div> 
+                </div>
+              </div>
+            </div>
+            <div className="b2">
+                <h1 style={{color:'black',marginTop:"0px"}}>Digital Cert-Chain</h1>
+                <div className="b2pic">
+                    <img src="https://cdn.coingape.com/wp-content/uploads/2023/05/25125224/unnamed-32.png" height="360px" width="500px"></img>
+                </div>
+                <div className="b2text">
+                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
+                The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. 
+                Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. 
+                Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+                </div>
+                <div id="b2btn">
+                    <button id="btn2">Get Started</button>
+                </div>
+            </div>
+            <div className="b3">
+                <h1 style={{marginTop:"0px"}}>Cert-Chain Vault</h1>
+                <div className="b3text">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                 Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                  It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+                It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                </div>
+                <div className="b3pic">
+                    <img src="https://pintu-academy.pintukripto.com/wp-content/uploads/2022/08/Perkembangan-blockchain-dari-masa-ke-masa.png" height="430px" width="600px"></img>
+                    
+                </div>
+                <div id="b3btn">
+                    <button id="btn3">Get Started</button>
+                </div>
+            </div>
+            <div className="b4">
+                <div className="b4pic">
+                    <img src="https://e0.pxfuel.com/wallpapers/890/238/desktop-wallpaper-bitcoin-golden-logo-yellow-brickwall-creative-cryptocurrency-bitcoin-3d-logo-bitcoin-logo-3d-art-bitcoin.jpg" height="300px" width="500px"></img>
+                </div>
+                <div id="b4s">
+                    <h3>How to use</h3>
+                    <h2>Certi-Chain</h2>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                        It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                         </p>
+                </div>
+                <div id="b41">
+                        <h2 style={{color:'brown'}}>FEATURES</h2>
+                        <p> is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
+                            The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. </p>
+                </div>
+                <div id="b42">
+                    <img src="https://flat-icons.com/wp-content/uploads/2022/07/Storage-Icon.png" height="100px" width="100px" id="img41"></img>
+                    <h3 id="mvh3">Safe Storage</h3>
+                    <p id="b4p1">This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+                </div>
+                <div id="b43">
+                <img src="https://static.vecteezy.com/system/resources/previews/005/655/157/original/adaptability-blue-gradient-concept-icon-social-entrepreneur-characteristic-abstract-idea-thin-line-illustration-flexibility-and-ability-to-transform-isolated-outline-color-drawing-vector.jpg" height="100px" width="100px" id="img42"></img>
+                    <h3>Adaptability</h3>
+                    <p id="b4p2">This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+                </div>
+            </div>
+            <div className="b5">
+                <h2 style={{fontFamily:'cursive',marginTop:"0px"}}>Events</h2>
+                <h1 style={{fontFamily:'sans-serif'}}>Explore Future Events</h1>
+                <div id="b51">
+                <div id="month1">
+                            <h3>October 2023</h3>
+                        </div>
+                        <div id="b51time">
+                        <img src="https://thumbs.dreamstime.com/b/golden-clock-icon-d-illustration-white-background-golden-clock-icon-d-illustration-116736669.jpg" height="60px" width="50px" id="clk1"></img>
+                            <h6>1.00 pm - 4.00pm</h6>
+                        </div>
+                    <div id="b51date">
+                        <h2 style={{fontSize:'54px'}}>6</h2> 
+                    </div>
+                   
+                    <hr style={{width:'250px'}}></hr>
+                    <div id="b51event">
+                         <h3 style={{fontFamily:'fantasy'}}>Political</h3>
+                        <h4 style={{fontFamily:'sans-serif'}}>  Politics of India  </h4>
+                        <h6 style={{fontSize:'14px'}}>Notify me</h6>
+                    </div>
+                </div>
+                <div id="b52">
+                <div id="month2">
+                            <h3>November 2023</h3>
+                        </div>
+                        <div id="b52time">
+                        <img src="https://thumbs.dreamstime.com/b/golden-clock-icon-d-illustration-white-background-golden-clock-icon-d-illustration-116736669.jpg" height="60px" width="50px" id="clk2"></img>
+                            <h6>3.00 pm - 5.00pm</h6>
+                        </div>
+                <div id="b52date">
+                <h2 style={{fontSize:'54px'}}>15</h2>
+                        </div>
+                        
+                        <hr style={{width:'250px'}}></hr>
+                        <div id="b52event">
+                        <h3 style={{fontFamily:'fantasy'}}>Digital India</h3>
+                        <h4 style={{fontFamily:'sans-serif'}}>  Understanding the emerging Technology evolving in India  </h4>
+                        <h6 style={{fontSize:'14px'}}>Notify me</h6>
+                    </div>
+                </div>
+                <div id="b53">
+                <div id="month3">
+                            <h3>December 2023</h3>
+                        </div>
+                        <div id="b53time">
+                            <img src="https://thumbs.dreamstime.com/b/golden-clock-icon-d-illustration-white-background-golden-clock-icon-d-illustration-116736669.jpg" height="60px" width="50px" id="clk3"></img>
+                            <h6 >6.00 pm - 10.00pm</h6>
+                        </div>
+                <div id="b53date">
+                <h2 style={{fontSize:'54px'}}>19</h2>
+                        </div>
+                        <hr style={{width:'250px'}}></hr>
+                        <div id="b53event">
+                        <h3 style={{fontFamily:'fantasy'}}>Harvest India </h3>
+                        <h4 style={{fontFamily:'sans-serif'}}>  Agricultural Impact of  India  </h4>
+                        <h6 style={{fontSize:'14px'}}>Notify me</h6>
+                    </div>
+                </div>
+                <button id="b5btn">Explore More</button>
+            </div>
+          </div> 
+    )
+}
+
+
+
+export default Arun;
