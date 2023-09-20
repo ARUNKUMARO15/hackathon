93fd30ab7d0d93cad52210073147d931ed6b2f9a
ARUNKUMARO15 <113664088+ARUNKUMARO15@users.noreply.github.com>
2023-09-20 23:33:07 +0530

    Add files via upload

---
+++ h.jsx
@@ -0,0 +1,152 @@
+import './h.css';
+
+
+function Arun()
+{
+    return (
+        <div className="blocks">
+            <div className="b1">
+                <div className='b1-container'>
+
+                <div id="b1c">
+              <div className="b1text">
+                <h4 style={{color:'red',textAlign:'left',fontFamily:'sans-serif'}}>CertiChain</h4>
+                <h1 style={{color:'white',textAlign:'left',fontFamily:'revert-layer'}}>Be Safe</h1>
+                <h1 style={{color:'white',textAlign:'left',fontFamily:'revert-layer'}}>Be Smart</h1>
+                <h5 style={{textAlign:'left',fontFamily:'sans-serif'}}>Save your Evidences Now !!</h5>
+                {/* <button id="qbtn"> </button> */}
+              </div>
+              <div id="b1pic">
+                <img src="https://mecaluxcom.cdnwm.com/blog/img/blockchain-exchange-industrial-information.1.2.jpg" height="400px" width="600px"></img>
+              </div> 
+                </div>
+              </div>
+            </div>
+            <div className="b2">
+                <h1 style={{color:'black',marginTop:"0px"}}>Digital Cert-Chain</h1>
+                <div className="b2pic">
+                    <img src="https://cdn.coingape.com/wp-content/uploads/2023/05/25125224/unnamed-32.png" height="360px" width="500px"></img>
+                </div>
+                <div className="b2text">
+                A digital certificate chain, also known as a certificate chain or certificate hierarchy, is a series of digital certificates that are used to verify the authenticity of a public key, typically associated with a particular entity or service on the internet.
+                 Digital certificate chains are a fundamental part of the public key infrastructure (PKI), which is used to secure communications and verify the identity of parties in various online transactions.
+                </div>
+                <div id="b2btn">
+                    <button id="btn2">Get Started</button>
+                </div>
+            </div>
+            <div className="b3">
+                <h1 style={{marginTop:"0px"}}>Cert-Chain Vault</h1>
+                <div className="b3text">
+                <h2>Certificate Chain and Vault Integration:</h2>
+
+
+                 In many secure systems, especially those involving digital certificates for authentication and encryption, a vault is used to store and manage cryptographic keys and associated digital certificates securely. This integration ensures that sensitive keys and certificates are protected from unauthorized access, tampering, or theft.
+                </div>
+                <div className="b3pic">
+                    <img src="https://pintu-academy.pintukripto.com/wp-content/uploads/2022/08/Perkembangan-blockchain-dari-masa-ke-masa.png" height="430px" width="600px"></img>
+                    
+                </div>
+                <div id="b3btn">
+                    <button id="btn3">Get Started</button>
+                </div>
+            </div>
+            <div className="b4">
+                <div className="b4pic">
+                    <img src="https://e0.pxfuel.com/wallpapers/890/238/desktop-wallpaper-bitcoin-golden-logo-yellow-brickwall-creative-cryptocurrency-bitcoin-3d-logo-bitcoin-logo-3d-art-bitcoin.jpg" height="300px" width="500px"></img>
+                </div>
+                <div id="b4s">
+                    <h3>How to use</h3>
+                    <h2>Certi-Chain</h2>
+                    <p>Using a certificate chain involves several steps, primarily in the context of secure communication over networks (e.g., HTTPS) and digital identity verification. Here's how to use a certificate chain 1.Obtain a Certificate Chain, 2.Install Root Certificate (Optional) ,3.Server-Side Configuration , 4.Client-Side Verification
+
+, 5.User Interface Indicators, 6.Maintaining Certificates, 7.Revocation Checking, 8.Logging and Monitoring
+                         </p>
+                </div>
+                <div id="b41">
+                        <h2 style={{color:'brown'}}>FEATURES</h2>
+                        <p > Its key features include the establishment of trust through a hierarchical model, enabling secure encryption and authentication. Certificate revocation mechanisms ensure the validity of certificates over time. Additionally, certificate chains play a vital role in verifying digital identities, ensuring the authenticity of websites, devices, and services. Effective key and certificate management throughout their lifecycle is essential for maintaining a secure Public Key Infrastructure (PKI) and the trustworthiness of certificates.</p>
+                </div>
+                <div id="b42">
+                    <img src="https://flat-icons.com/wp-content/uploads/2022/07/Storage-Icon.png" height="100px" width="100px" id="img41"></img>
+                    <h3 id="mvh3" style={{color:'brown'}}>Safe Storage</h3>
+                    <p id="b4p1" >Safe storage is the foundation of data security, ensuring that sensitive information remains protected. It encompasses creating a physically and logically secure environment where data is shielded from unauthorized access, theft, and damage. Encryption is applied to data both in transit and at rest, safeguarding its confidentiality. Access controls and authentication mechanisms are implemented to restrict data access to authorized users with appropriate permissions. 
+                    {/* {/* Regular backups and comprehensive disaster recovery plans are essential to mitigate data loss risks. Continuous monitoring and auditing of storage systems ensure that data remains safe, compliant with security policies, and resilient against potential threats. */}
+                    </p> 
+                </div>
+                <div id="b43">
+                <img src="https://static.vecteezy.com/system/resources/previews/005/655/157/original/adaptability-blue-gradient-concept-icon-social-entrepreneur-characteristic-abstract-idea-thin-line-illustration-flexibility-and-ability-to-transform-isolated-outline-color-drawing-vector.jpg" height="160px" width="160px" id="img42"></img>
+                    <h3 style={{color:'brown'}} >Adaptability</h3>
+                    <p id="b4p2">The adaptability of a certificate chain is its inherent ability to flexibly accommodate changes and evolving requirements in the field of cybersecurity and digital trust. This adaptability is evident in its hierarchical trust model, which allows for the integration of new trusted entities and the establishment of trust in diverse environments. Certificate chains find utility across various industries, seamlessly adapting to secure web communications, email encryption, code signing, and IoT device authentication. 
+                    {/* {/* They exhibit cross-platform compatibility, ensuring they can be employed in heterogeneous ecosystems, and can readily integrate with emerging technologies and cryptographic standards as security demands evolve.  */}
+                    </p>
+                </div>
+            </div>
+            <div className="b5">
+                <h2 style={{fontFamily:'cursive',marginTop:"0px"}}>Events</h2>
+                <h1 style={{fontFamily:'sans-serif'}}>Explore Future Events</h1>
+                <div id="b51">
+                <div id="month1">
+                            <h3>October 2023</h3>
+                        </div>
+                        <div id="b51time">
+                        <img src="https://thumbs.dreamstime.com/b/golden-clock-icon-d-illustration-white-background-golden-clock-icon-d-illustration-116736669.jpg" height="60px" width="50px" id="clk1"></img>
+                            <h6>1.00 pm - 4.00pm</h6>
+                        </div>
+                    <div id="b51date">
+                        <h2 style={{fontSize:'54px'}}>6</h2> 
+                    </div>
+                   
+                    <hr style={{width:'250px'}}></hr>
+                    <div id="b51event">
+                         <h3 style={{fontFamily:'fantasy'}}>Block-Chain</h3>
+                        <h4 style={{fontFamily:'sans-serif'}}> Block Chain Technology #1  </h4>
+                        <h6 style={{fontSize:'14px'}}>Notify me</h6>
+                    </div>
+                </div>
+                <div id="b52">
+                <div id="month2">
+                            <h3>November 2023</h3>
+                        </div>
+                        <div id="b52time">
+                        <img src="https://thumbs.dreamstime.com/b/golden-clock-icon-d-illustration-white-background-golden-clock-icon-d-illustration-116736669.jpg" height="60px" width="50px" id="clk2"></img>
+                            <h6>3.00 pm - 5.00pm</h6>
+                        </div>
+                <div id="b52date">
+                <h2 style={{fontSize:'54px'}}>15</h2>
+                        </div>
+                        
+                        <hr style={{width:'250px'}}></hr>
+                        <div id="b52event">
+                        <h3 style={{fontFamily:'fantasy'}}>Digital India</h3>
+                        <h4 style={{fontFamily:'sans-serif'}}>  Understanding the emerging Technology evolving in India  </h4>
+                        <h6 style={{fontSize:'14px'}}>Notify me</h6>
+                    </div>
+                </div>
+                <div id="b53">
+                <div id="month3">
+                            <h3>December 2023</h3>
+                        </div>
+                        <div id="b53time">
+                            <img src="https://thumbs.dreamstime.com/b/golden-clock-icon-d-illustration-white-background-golden-clock-icon-d-illustration-116736669.jpg" height="60px" width="50px" id="clk3"></img>
+                            <h6 >6.00 pm - 10.00pm</h6>
+                        </div>
+                <div id="b53date">
+                <h2 style={{fontSize:'54px'}}>19</h2>
+                        </div>
+                        <hr style={{width:'250px'}}></hr>
+                        <div id="b53event">
+                        <h3 style={{fontFamily:'fantasy'}}>De-Centralized India </h3>
+                        <h4 style={{fontFamily:'sans-serif'}}>  De-Centralized Impact of  India  </h4>
+                        <h6 style={{fontSize:'14px'}}>Notify me</h6>
+                    </div>
+                </div>
+                <button id="b5btn">Explore More</button>
+            </div>
+          </div> 
+    )
+}
+
+
+
+export default Arun;
